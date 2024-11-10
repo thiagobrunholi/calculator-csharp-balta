ef01ed07d722a609febf2371fb7342e9de1b57fa
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-10 18:44:35 -0300

    Menu da Aplicação
    Criando um Menu de opções para nosso projeto.

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb9778a3b18913d6eb83dbc0b9903bb28a1ecad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d49ccf981066ebe9d7904251932c68da78151e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
