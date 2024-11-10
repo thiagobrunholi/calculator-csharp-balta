2e81abe74149103e11173c872aaa74c93a494ca9
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-10 16:07:04 -0300

    Utilizando funções
    Definindo a Função Soma fora do nosso Main, e chamando a função para o programa.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,10 +2,15 @@
 {
     private static void Main(string[] args)
     {
+        Soma();
+    }
+
+    static void Soma()
+    {
         Console.Clear();
         Console.WriteLine("Primeiro valor:");
         float v1 = float.Parse(Console.ReadLine()!);
-        
+
         Console.WriteLine("Segundo Valor:");
         float v2 = float.Parse(Console.ReadLine()!);
 
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4658d2c47daa664199888eb23e9bc77c58d0f700")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa63e6609a88c0469cfe2b82035a7c308b8d108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
