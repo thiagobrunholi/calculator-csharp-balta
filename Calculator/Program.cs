4559c6234609103249a9d75d65f8ac98784bd58a
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-10 16:21:02 -0300

    Subtração
    Adicionando a Função de Subtração ao nosso Projeto.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,7 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Soma();
+        //Soma();
+        Subtracao();
     }
 
     static void Soma()
@@ -18,5 +19,23 @@
 
         float resultado = v1 + v2;
         Console.WriteLine($"O resultado da soma é: {resultado}");
+        Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("");
+
+        float resultado = v1 - v2;
+        Console.WriteLine($"O resultado da subtração é: {resultado}");
+        Console.ReadKey();
     }
 }
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa63e6609a88c0469cfe2b82035a7c308b8d108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e81abe74149103e11173c872aaa74c93a494ca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
