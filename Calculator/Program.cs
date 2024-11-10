4658d2c47daa664199888eb23e9bc77c58d0f700
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-10 15:49:32 -0300

    Iniciando o Projeto
    Criando o Projeto Console via Terminal e adicionando os primeiros códigos.

---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine()!);
+
+        Console.WriteLine(v1);
+    }
+}
